3587a8bdee2ac099d89e3e7e817c3a94d4a9c8c3
PaulT1983 <pault83@gmail.com>
2024-03-14 20:48:04 +0200

    Classwork #7 without Homework TODOs

---
+++ src/telran/strings/test/RegularExpressionsTests.java
@@ -0,0 +1,62 @@
+package telran.strings.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.strings.RegularExpressions;
+
+class RegularExpressionsTests {
+
+	@Test
+	void javaVariableTrueTest() {
+		String regex = RegularExpressions.javaVariable();
+		assertTrue("abs".matches(regex));
+		assertTrue("a".matches(regex));
+		assertTrue("$".matches(regex));
+		assertTrue("$123".matches(regex));
+		assertTrue("$1_23".matches(regex));
+		assertTrue("abs_".matches(regex));
+		assertTrue("__".matches(regex));
+		assertTrue("Abs_".matches(regex));
+	}
+	
+	@Test
+	void javaVariableFalseTest() {
+		String regex = RegularExpressions.javaVariable();
+		assertFalse("1abs".matches(regex));
+		assertFalse("_".matches(regex));
+		assertFalse("-".matches(regex));
+		assertFalse("a-2".matches(regex));
+		assertFalse("a 2".matches(regex));
+		assertFalse("a?2".matches(regex));
+		assertFalse("a?2".matches(regex));
+		assertFalse("i*nt".matches(regex));
+	}
+	
+	@Test 
+	void zero_300TrueTest() {
+		String regex = RegularExpressions.zero_300();
+		assertTrue("0".matches(regex));
+		assertTrue("1".matches(regex));
+		assertTrue("19".matches(regex));
+		assertTrue("198".matches(regex));
+		assertTrue("299".matches(regex));
+		assertTrue("30".matches(regex));
+		assertTrue("33".matches(regex));
+		assertTrue("300".matches(regex));
+	}
+
+	@Test 
+	void zero_300FalseTest() {
+		String regex = RegularExpressions.zero_300();
+		assertFalse("00".matches(regex));
+		assertFalse("01".matches(regex));
+		assertFalse("19s".matches(regex));
+		assertFalse("398".matches(regex));
+		assertFalse("2990".matches(regex));
+		assertFalse("-30".matches(regex));
+		assertFalse("330".matches(regex));
+		assertFalse("301".matches(regex));
+	}
+}
