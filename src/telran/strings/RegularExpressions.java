4247478269f3a7e42092b6f2967f147121a3aec2
PaulT1983 <pault83@gmail.com>
2024-03-17 12:40:37 +0200

    Homework #7 with ipOctet()

--- src/telran/strings/RegularExpressions.java
+++ src/telran/strings/RegularExpressions.java
@@ -14,21 +14,10 @@ public class RegularExpressions {
 	}
 	
 	public static String ipOctet() {
-		//TODO
 		//contains 1 - 3 symbols presenting number from 0 - 255; 
 		//leading zeros are allowed
+		String regex = "[0-1]\\d{0,2}|2[0-4]\\d|25[0-5]";
 		
-		return null;
-	}
-	
-	public static String mobileIsraelPhone() {
-		//TODO
-		//string contains possible Israel mobile phone number
-		//+972-(maybe without '-' but no space)<prefix two digit beginning from 5>-<7 digits of number>
-		//<prefix three digits: first - 0, second 5, third - any>-(maybe without '-' but no space)<7 digits of number>
-		//E.g. 054-733-44-58, 054-73344-58, 054-7334458, 054733-44-58, 0547334458
-		//E.g. +972547334458, +972-547334458, +972-54-733-4458
-		
-		return null;
+		return regex;
 	}
 }
